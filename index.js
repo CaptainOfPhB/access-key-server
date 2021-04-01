1f9e25177a8d047ed1af766e9ac5035a749b0700
CaptainOfPhB <captaininphw@gmail.com>
2021-04-01 19:04:22 +0800

    add charset

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function handleRequest(event) {
     }),
     {
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json; charset=UTF-8',
         'Access-Control-Allow-Origin': '*'
       }
     });
